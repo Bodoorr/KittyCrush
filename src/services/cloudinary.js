ddb8787cb4fe2276438f7681ec28654e573f0c10
Bodoor <Bodoor5@hotmail.com>
2026-02-14 07:47:30 +0300

    update cloudinary variables

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
-const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+const CLOUD_NAME = import.meta.env.CLOUDINARY_CLOUD_NAME
+const UPLOAD_PRESET = import.meta.env.CLOUDINARY_UPLOAD_PRESET
 
 export const uploadToCloudinary = async (imageData) => {
   const formData = new FormData()
